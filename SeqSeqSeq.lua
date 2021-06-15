fea66a5ad18d9b5f02becc18c57f3a5d56043f6c
davidalovell <davidlovell@live.co.uk>
2021-06-15 20:01:41 +0100

    Removed default action from Voice:action(val)

--- SeqSeqSeq.lua
+++ SeqSeqSeq.lua
@@ -69,7 +69,6 @@ function Voice:new(on, ext_octave, ext_degree, level, octave, degree, transpose,
   end
 
   o.action = function(self, val)
-    self.seq[1].mod.division = val
   end
 
   o.new_note = function(self)
@@ -241,6 +240,9 @@ function init()
 
   v[3] = Voice:new(true, true, true, 0.5, 1, 3, 0, function(note, level) ii.wsyn.play_note(note, level) end)
   v[3]:new_seq(1, true, {3,1, 3,1, 3,1, 2,1,1}, 1, 1, 'next', true)
+  v[3].action = function(self, val)
+    self.seq[1].mod.division = val
+  end
 
   v[4] = Voice:new(true, false, true, 1.5, -2, 1, 0, function(note, level) ii.jf.play_voice(1, note, level) end)
   v[4]:new_seq(1, true, {4,3,1}, 1, 1, 'next', true)
