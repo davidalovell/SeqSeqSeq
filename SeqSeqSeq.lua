564148ed75a052e9e868ae8cd9a590575912bbfb
davidalovell <davidlovell@live.co.uk>
2021-06-16 22:20:40 +0100

    Song settings added

--- SeqSeqSeq.lua
+++ SeqSeqSeq.lua
@@ -212,38 +212,46 @@ function init()
   trigger_reset = new_divider(function() global.reset = true end)
   clock_divider = new_divider(function() output[1](pulse(0.01)) on_division() end)
 
-  -- declare voices/sequencers/actions, e.g.
-  -- v = {}
-  -- v.trig1 = Voice:new(true, true, true, 1, 0, 1, 0)
-  --
-  -- v.trig2 = Voice:new(true, true, true, 1, 0, 5, 0)
-  --
-  -- v.trig3 = Voice:new(true, true, true, 2, 1, 1, 0)
-  -- v.trig3:new_seq(1, true, {1,3,5}, 1, 1, 'next', true)
-  -- v.trig3:new_seq(2, true, {2,3,3}, 1, 1, 'next')
-  -- function v.trig3:action(val)
-  --   self.mod.degree = val
-  --   self.seq[1].mod.division = self:play_seq(2)
-  -- end
-  --
-  -- v.div1 = Voice:new(true, false, false, 0.5, 1, 1, 0, function(note, level) ii.wsyn.play_note(note, level) end)
-  -- v.div1:new_seq(1, true, {1,2,3,4,5,6,7}, 1, 2, 'next', true)
-  -- v.div1:new_seq(2, true, {1,3,5}, 4, 1, 'next')
-  -- v.div1:new_seq(3, true, {2,3,1}, 1, 1, 'next')
-  -- function v.div1:action(val)
-  --   self.mod.degree = val + self:play_seq(2)
-  --   self.seq[1].mod.division = self:play_seq(3)
-  -- end
-  --
-  -- v.div2 = Voice:new(true, false, true, 1.5, -2, 1, 0, function(note, level) ii.jf.play_voice(1, note, level) end)
-  -- v.div2:new_seq(1, true, {4,3,1}, 1, 1, 'next', true)
-  -- v.div2:new_seq(2, true, {1,1,1}, 1, 1, 'next')
-  -- function v.div2:action(val)
-  --   self.seq[1].mod.division = val * selector(txi.param[3], div.x2, 0, 10)
-  --   self.seq[1].sequence = selector(txi.param[4], {{4,3,1}, {2,1/2,1/2,1}}, 0, 10)
-  --   self.seq[2].sequence[3] = math.random(3,4)
-  --   self.mod.degree = self:play_seq(2)
-  -- end
+  ii.jf.run_mode(1)
+  ii.jf.run(5)
+
+  output[2](lfo(8,5,'sine'))
+
+  v = {}
+
+  v[1] = Voice:new(true, true, true, 0.5, -1, 1, 0)
+  v[1]:new_seq(1, true, {1,2,3,1/2}, 1, 1, 'next', true)
+  v[1]:new_seq(2, true, {true,false},1, 1, 'next')
+  v[1].action = function(self, val)
+    self.seq[1].mod.division = val * selector(txi.param[2], div.even, 0, 10)
+    self.mod.on = self:play_seq(2)
+    self.seq[2].mod.division = selector(txi.param[1], div.x2, 0, 10)
+  end
+
+  v[2] = Voice:new(true, true, true, 0.5, -2, 5, 0)
+  v[2]:new_seq(1, true, {1,5}, 1, 1, 'next', true)
+  v[2]:new_seq(2, true, {true,false},1, 1, 'next')
+  v[2].action = function(self, val)
+    self.seq[1].mod.division = val * selector(txi.param[2], div.odd, 0, 10)
+    self.mod.on = self:play_seq(2)
+    self.seq[2].mod.division = selector(txi.param[1], div.x2, 0, 10)
+  end
+
+  v[3] = Voice:new(true, true, true, 0.5, 1, 3, 0, function(note, level) ii.wsyn.play_note(note, level) end)
+  v[3]:new_seq(1, true, {3,1, 3,1, 3,1, 2,1,1}, 1, 1, 'next', true)
+  v[3].action = function(self, val)
+    self.seq[1].mod.division = val
+  end
+
+  v[4] = Voice:new(true, false, true, 1.5, -2, 1, 0, function(note, level) ii.jf.play_voice(1, note, level) end)
+  v[4]:new_seq(1, true, {4,3,1}, 1, 1, 'next', true)
+  v[4]:new_seq(2, true, {1,1,1}, 1, 1, 'next')
+  v[4].action = function(self, val)
+    self.seq[1].mod.division = val * selector(txi.param[3], div.x2, 0, 10)
+    self.seq[1].sequence = selector(txi.param[4], {{4,3,1}, {2,1/2,1/2,1}}, 0, 10)
+    self.seq[2].sequence[3] = math.random(3,4)
+    self.mod.degree = self:play_seq(2)
+  end
 end
 
 function txi_getter()
@@ -267,11 +275,10 @@ end
 
 input[2].change = function()
   trigger_reset(global.count)
-  
-  -- voices/seqeuncers to play on trigger to crow input[2]
-  -- v.trig1:play_voice()
-  -- v.trig2:play_voice()
-  -- v.trig3:play_seq()
+
+  for i = 1, 4 do
+    v[i]:play_seq()
+  end
 
   global.reset = false
 end
@@ -281,10 +288,9 @@ function on_clock()
 
   txi_getter()
 
-  -- variables to be set every clock pulse, e.g.
-  -- global.bpm = linlin(txi.input[1], 0, 5, 10, 3000)
-  -- global.division = selector(txi.input[2], div.x2, 0, 4)
-  -- global.negharm = selector(txi.input[3], {false,true}, 0, 4)
+  global.bpm = linlin(txi.input[1], 0, 5, 10, 3000)
+  global.division = selector(txi.input[2], div.x2, 0, 4)
+  global.negharm = selector(txi.input[3], {false,true}, 0, 4)
 
   metro[1].time = 60/global.bpm
   clock_divider(global.division)
@@ -293,7 +299,4 @@ function on_clock()
 end
 
 function on_division()
-  -- voices/sequencers to play on every clock division
-  -- v.div1:play_seq()
-  -- v.div2:play_seq()
 end
