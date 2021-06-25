3d074fb00d7e89634f46ec47a72c4202e1699fed
davidalovell <davidlovell@live.co.uk>
2021-06-25 22:40:58 +0100

    example template

--- example_template.lua
+++ example_template.lua
@@ -6,19 +6,18 @@
 -- declare voices/sequencers/actions
 
 
-
 --------------------------------------------------------
 -- paste below inside:
 -- input[2].change = function()
 -- and below the following line:
 -- voices/seqeuncers to play on trigger to crow input[2]
 
+
 --------------------------------------------------------
 -- paste below inside:
 -- function on_clock()
 -- and below the following line:
 -- variables to be set on clock, e.g.
-
 
 
 --------------------------------------------------------
